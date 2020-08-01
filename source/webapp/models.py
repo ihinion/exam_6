53e1e097dbc08a93bcdf1e674ebe6b7e94e7c312
ilia <jamiashvili@gmail.com>
2020-08-01 11:41:54 +0600

    Created model Entry, registered in admin.py, migrated model to database, created dump with test entries and superuser

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('active', 'Active'),
+    ('blocked', 'Blocked')
+)
+
+DEFAULT_STATUS = STATUS_CHOICES[0][0]
+
+
+class Entry(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Name')
+    mail = models.EmailField(max_length=30, null=False, blank=False, verbose_name='Email')
+    text = models.CharField(max_length=500, null=False, blank=False, verbose_name='Text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=DEFAULT_STATUS, verbose_name='Status')
+
+    def __str__(self):
+        return f'{self.pk}.By: {self.name}. Status: {self.status}. Created at: {self.created_at}'
